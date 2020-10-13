719355e4455c742d458c36b6a970ee886c170863
luigiPerrelli <72208147+luigiPerrelli@users.noreply.github.com>
2020-10-13 15:54:57 -0300

    Update views.py

--- deals/views.py
+++ deals/views.py
@@ -1,20 +1,5 @@
 from django.shortcuts import render
 import funcoes
-import json
-from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
-from django.views.decorators.http import require_POST
-
-@csrf_exempt
-@require_POST
-def webhook_endpoint(request):
-    jsondata = request.body
-    data = json.loads(jsondata)
-    for answer in data['form_response']['answers']: # go through all the answers
-      type = answer['type']
-      print(f'answer: {answer[type]}') # print value of answers
-
-    return HttpResponse(status=200
 
 
 def index(request):
